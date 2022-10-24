20c5c9b31a582d259d0b7a89cc8c10001d399888
Eduardo Marchena <edumar111@gmail.com>
2022-10-24 15:28:17 -0500

    pubic nodes

---
+++ resources/js/list.js
@@ -0,0 +1,83 @@
+const spanError = document.getElementById('error')
+
+async function loadList() {
+    const API_URL_NODES_LIST = 'http://localhost:5002/nodes/';
+   
+    var optionUrl = {  
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Origin': '*',
+          
+        }
+      }
+  
+  
+    const res = await fetch(API_URL_NODES_LIST,optionUrl);
+    var table = document.getElementById("tableNodes");
+    
+    
+      if(res.status == 204){
+        spanError.innerHTML = " Sin contenido: " + res.status ;
+      
+      } else if (res.status == 200 ){
+        const data = await res.json();
+        console.log(data)
+        console.log(res.status)
+
+        let row = table.insertRow(0);
+        let cell1 = row.insertCell(0);
+        let cell2 = row.insertCell(1);
+        let cell3 = row.insertCell(2);
+        let cell4 = row.insertCell(3);
+        let cell5 = row.insertCell(4);
+        let cell6 = row.insertCell(5);
+        let cell7 = row.insertCell(6);
+        let cell8 = row.insertCell(7);
+    
+        cell1.innerHTML = "count"
+        cell2.innerHTML = "Entity"
+        cell3.innerHTML = "Network ID"
+        cell4.innerHTML = "Network Name"
+        cell5.innerHTML = "Type"
+        cell6.innerHTML = "Technical Contact"
+        cell7.innerHTML = "Business contact"
+        cell8.innerHTML = "Node ID"
+
+        let j =1
+
+        data.forEach(node => {
+          console.log(node)
+         
+           
+  
+            let row = table.insertRow(j);
+            let cell1 = row.insertCell(0);
+            let cell2 = row.insertCell(1);
+            let cell3 = row.insertCell(2);
+            let cell4 = row.insertCell(3);
+            let cell5 = row.insertCell(4);
+            let cell6 = row.insertCell(5);
+            let cell7 = row.insertCell(6);
+            let cell8 = row.insertCell(7);
+
+           
+           
+            cell1.innerHTML = j,
+            cell2.innerHTML = node.entity,
+            cell3.innerHTML = node.networkId;
+            cell4.innerHTML = node.networkName;
+            cell5.innerHTML = node.type;
+            cell6.innerHTML = node.technicalContact;
+            cell7.innerHTML = node.BussinesContact;
+            cell8.innerHTML = node.enode;
+
+          j ++
+        });
+      }else if(res.status !== 200){
+        spanError.innerHTML = "Hubo un error: " + res.status + res.error;
+      }
+  }
+
+  loadList()
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const nodesRouter = require('./routes/nodes.routes')
+const  path  = require("path");
+const fs = require('fs');
 // config server express
 const app = express()
 const PORT = process.env.SEVER_PORT || 5002
@@ -9,6 +11,7 @@ const MONGODB_URI=process.env.MONGODB_URI || "mongodb://admin:password@localhost
 app.use(express.json())
 
 console.log(`URI_MONGO ${MONGODB_URI}`)
+app.use('/static', express.static(path.join(__dirname, 'resources')))
 //DB connection
 mongoose.connect(MONGODB_URI,{
    // useCreateIndex: true,
@@ -16,7 +19,9 @@ mongoose.connect(MONGODB_URI,{
     useUnifiedTopology: true
 })
 
-
+app.get('/list-nodes', function(req, res) {
+    res.sendFile(path.join(__dirname, './resources/pages/listnodes.html'));
+  });
 // Routes
 app.get("/healt",(req,res)=>{
     res.send("up")
